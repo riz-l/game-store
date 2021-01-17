e27467aba8293121dfb2e0e6a4c5d14b685bf22c
riz-l <joshua.riz.layton@gmail.com>
2021-01-17 17:10:46 +0000

    MASTER: Adding configured ProfileScreen

--- src/screens/ProfileScreen/ProfileScreen.component.jsx
+++ src/screens/ProfileScreen/ProfileScreen.component.jsx
@@ -1,8 +1,27 @@
 // Import: Dependencies
 import React from "react";
+import { StatusBar } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+
+// Import: Data
+import games from "../../../data/gameData";
 
 // Import: Elements
-import { Container } from "./ProfileScreen.elements";
+import {
+  Account,
+  Avatar,
+  AvatarContainer,
+  Badge,
+  Container,
+  Game,
+  GameImage,
+  Games,
+  Info,
+  Logout,
+  Settings,
+  Stat,
+  Stats,
+} from "./ProfileScreen.elements";
 
 // Import: Components
 import { Text } from "../../components";
@@ -11,7 +30,66 @@ import { Text } from "../../components";
 export default function ProfileScreen() {
   return (
     <Container>
-      <Text>Profile Screen</Text>
+      <StatusBar backgroundColor="#343434" barStyle="light-content" />
+
+      <AvatarContainer>
+        <Avatar source={require("../../../assets/images/avatar.png")} />
+        <Text large bold>
+          riz-l
+        </Text>
+      </AvatarContainer>
+
+      <Badge>
+        <Text small heavy>
+          Amateur
+        </Text>
+      </Badge>
+
+      <Stats>
+        <Stat>
+          <Text large heavy>
+            250 <Text color="#9a9a9a">Games</Text>
+          </Text>
+        </Stat>
+
+        <Stat>
+          <Text large heavy>
+            9 <Text color="#9a9a9a">Purchases</Text>
+          </Text>
+        </Stat>
+      </Stats>
+
+      <Text center large>
+        Purchased Games
+      </Text>
+
+      <Games
+        data={games}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => (
+          <Game>
+            <GameImage source={item.cover} />
+            <Info>
+              <Text bold>{item.title}</Text>
+              <Text small color="#9a9a9a">
+                {Math.floor(Math.random() * 1000) + 1} Sales
+              </Text>
+            </Info>
+            <Text heavy color="#819ee5">
+              £{Math.floor(Math.random() * 50) + 1}
+            </Text>
+          </Game>
+        )}
+      />
+
+      <Account>
+        <Settings>
+          <Entypo name="cog" size={24} color="#ffffff" />
+        </Settings>
+        <Logout>
+          <Text heavy>Logout</Text>
+        </Logout>
+      </Account>
     </Container>
   );
 }
--- src/screens/ProfileScreen/ProfileScreen.elements.js
+++ src/screens/ProfileScreen/ProfileScreen.elements.js
@@ -1,5 +1,90 @@
 // Import: Dependencies
 import styled from "styled-components/native";
 
+// Element: Account
+export const Account = styled.View`
+  flex-direction: row;
+  margin: 32px 16px;
+`;
+
+// Element: Avatar
+export const Avatar = styled.Image`
+  height: 100px;
+  margin-bottom: 16px;
+  width: 100px;
+`;
+
+// Element: AvatarContainer
+export const AvatarContainer = styled.View`
+  align-items: center;
+  margin: 64px 0 16px 0;
+`;
+
+// Element: Badge
+export const Badge = styled.View`
+  align-self: center;
+  background-color: #819ee5;
+  border-radius: 8px;
+  padding: 4px 12px;
+`;
+
 // Element: Container
-export const Container = styled.View``;
+export const Container = styled.View`
+  background-color: #343434;
+  flex: 1;
+`;
+
+// Element: Game
+export const Game = styled.View`
+  align-items: center;
+  flex-direction: row;
+  margin-bottom: 16px;
+`;
+
+// Element: GameImage
+export const GameImage = styled.Image`
+  border-radius: 12px;
+  height: 64px;
+  width: 64px;
+`;
+
+// Element: Games
+export const Games = styled.FlatList`
+  margin: 32px 16px 0 16px;
+`;
+
+// Element: Info
+export const Info = styled.View`
+  flex: 1;
+  margin: 0 16px;
+`;
+
+// Element: Logout
+export const Logout = styled.TouchableOpacity`
+  align-items: center;
+  background-color: #404040;
+  border-radius: 8px;
+  flex: 1;
+  justify-content: center;
+  margin-left: 16px;
+`;
+
+// Element: Settings
+export const Settings = styled.TouchableOpacity`
+  background-color: #819ee5;
+  border-radius: 8px;
+  padding: 12px;
+`;
+
+// Element: Stat
+export const Stat = styled.View`
+  margin: 0 16px;
+`;
+
+// Element: Stats
+export const Stats = styled.View`
+  align-items: center;
+  align-self: center;
+  flex-direction: row;
+  margin: 32px 0;
+`;
